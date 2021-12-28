f5e62c520ddf444a06b61afd205681fa75eb6aed
Gulten Ulukal <78637627+gltnlkl@users.noreply.github.com>
2021-12-28 23:56:50 +0300

    web project

---
+++ js/jquery-exam.js
@@ -0,0 +1,32 @@
+//vki hesaplama
+$(function () {
+  $("#vki_hesapla").on("click", function () {
+    let kilo = Number($("#kilo").val());
+    let boy = Number($("#boy").val()) / 100;
+    let vki = Math.round(kilo / Math.pow(boy, 2));
+
+    if (vki < 18) {
+      $("#sonuc").html("ortalamanız " + vki + " Çok zayıfsınız");
+    } else if (18 <= vki && vki < 21) {
+      $("#sonuc").html("ortalamanız " + vki + " orta kilo");
+    } else if (21 <= vki && vki < 24) {
+      $("#sonuc").html("ortalamanız " + vki + "  kilolu");
+    } else if (vki >= 24) {
+      document.getElementById("sonuc").innerHTML =
+        "ortalamanız " + vki + "  çok kilonuz bulunuyor";
+    }
+
+    $("#ortalama").html(vki);
+    console.log(vki);
+  });
+});
+
+function javascript_codes() {
+  let kilo = Number(document.getElementById("kilo").value);
+  let boy = Number(document.getElementById("boy").value) / 100;
+  let vki = Math.round(kilo / Math.pow(boy, 2));
+  console.log("JavaScript : " + vki);
+  document.getElementById("ortalama").value = "<b>" + vki + "</b>";
+}
+
+
